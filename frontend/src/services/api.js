e91bbbe44a208d71aed35d6f89a7662ec47782c9
Shiv Prakash <shivprakash.cse2027@gmail.com>
2026-03-29 08:05:50 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // ✅ FIX
+  baseURL: 'https://judiciary-case-backlog-reduction-backend.onrender.com/api', // ✅ FIX
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
